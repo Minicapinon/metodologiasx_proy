c3d9d5ed5ab2db88f75a7554fbd0d571f8dade29
Minicapinon <bava080602hgtrtna6@cbtis217.edu.mx>
2024-10-28 08:39:00 -0600

    Archivos vacios

---
+++ proyecto_metodoligias/MainForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace proyecto_metodoligias
+{
+	/// <summary>
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
